a728a50f25285548614e00a42fb5c8554f2abb0e
Dominik Nsosso <dominik.nsosso@immediate.co.uk>
2022-04-04 05:18:35 +0200

    calculate single manning for a day

---
+++ src/Utils/SingleManningCalculator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Utils;
+
+use App\Model\SegmentInterface;
+
+class SingleManningCalculator
+{
+    public function __construct(
+        private MergeSegments $mergeSegments,
+        private SegmentInSegment $segmentInSegment
+    )
+    {
+    }
+
+    /**
+     * @param SegmentInterface[] $shifts
+     * @return int
+     */
+    public function calculate(array $shifts): int
+    {
+        $segmentedTimeline = $this->mergeSegments->merge(...$shifts);
+
+        $duration = 0;
+        foreach ($segmentedTimeline as $segment) {
+            $segmentCoveredByShift = 0;
+            foreach ($shifts as $shift) {
+                if ($this->segmentInSegment->isSegmentInSegment($segment, $shift)) {
+                    $segmentCoveredByShift++;
+                    if (1 < $segmentCoveredByShift) {
+                        break;
+                    }
+                }
+            }
+
+            if (1 === $segmentCoveredByShift) {
+                $duration += $segment->getEnd() - $segment->getStart();
+            }
+        }
+
+        return $duration;
+    }
+}
---
+++ tests/Integration/Utils/SingleManningCalculatorTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests\Integration\Utils;
+
+use App\Model\Segment;
+use App\Utils\SingleManningCalculator;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class SingleManningCalculatorTest extends KernelTestCase
+{
+    private $singleManningCalculator;
+
+    public function setUp(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $this->singleManningCalculator = $container->get(SingleManningCalculator::class);
+    }
+
+    /**
+     * @param array $shifts
+     * @param int $expected
+     * @dataProvider calculateDataProvider
+     */
+    public function testCalculate(array $shifts, int $expected): void
+    {
+        $this->assertEquals($expected, $this->singleManningCalculator->calculate($shifts));
+    }
+
+    public function calculateDataProvider(): array
+    {
+        return [
+            'Scenario One - Single Shift' => [
+                [
+                    new Segment(0, 22)
+                ],
+                22
+            ],
+            'Scenario Two - After Each Other' => [
+                [
+                    new Segment(0, 10),
+                    new Segment(10, 23)
+                ],
+                23
+            ],
+            'Scenario Three - Lap over' => [
+                [
+                    new Segment(0, 12),
+                    new Segment(10, 27)
+                ],
+                25
+            ],
+        ];
+    }
+}
